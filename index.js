02499ef2533cca8940f8a325292ca9910a691c71
Mike Kang <mike.kang@mail.utoronto.ca>
2020-07-08 23:44:00 -0400

    passport fixes

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: 'http://localhost:5000/auth/google/callback'
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
-const PORT = process.env.PORT;
+app.get('/auth/google', passport.authenticate('google',{
+  scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+app.get('/', (req, res) => {
+  res.send('hi');
+})
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
